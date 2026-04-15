9cdfa0c4a62916594bde583fcf960090f98ee878
Mollianeta <mollianeta@gmail.com>
2026-04-14 20:57:25 -0600

    formatWeight should now also work

--- src/components/handbook/itemFormatter.ts
+++ src/components/handbook/itemFormatter.ts
@@ -2,13 +2,12 @@
 function formatCost(rawCost : string): string {
     const goldValue = 100;
     const silverValue = 10;
-    const copperValue = 1;
 
-    const numberCost = Number(rawCost) * goldValue;
+    const numberCost = Number(rawCost) * 100;
 
     const goldCost = Math.floor(numberCost / goldValue);
     const silverCost = Math.floor((numberCost % goldValue) / silverValue);
-    const copperCost = Math.floor((numberCost % silverValue) / copperValue);
+    const copperCost = Math.floor(numberCost % silverValue);
 
     let convertedCost = ``;
 
@@ -20,7 +19,19 @@ function formatCost(rawCost : string): string {
 }
 
 function formatWeight(rawWeight : string): string {
+    const numberWeight = Number(rawWeight) * 100;
 
+    const wholePounds = Math.floor(numberWeight / 100);
+    const halfPounds = Math.floor((numberWeight % 100) / 50);
+    const quarterPounds = Math.floor((numberWeight % 50) / 25);
 
+    let convertedWeight = ``;
 
+    if (wholePounds > 0) convertedWeight += `${wholePounds} lbs `;
+    if (halfPounds > 0) convertedWeight += `${halfPounds}/2 lbs `;
+    if (quarterPounds > 0) convertedWeight += `${quarterPounds}/4 lbs `;
+
+    return convertedWeight;
 }
+
+export { formatCost, formatWeight }
